0db0f605b4d290e12616a990b17f208ad758c8a2
sergey.k <k.sergey2010@yandex.ru>
2022-04-25 20:21:47 +0700

    zadacha_36 done

---
+++ zadacha_36/Program.cs
@@ -0,0 +1,31 @@
+﻿void Random (int[]arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(-1000, 1000);
+    }
+}
+
+void Print (int[]arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int SumNeChet (int[]arr)
+{
+    int result = 0;
+    for (int i = 0; i < arr.Length; i+=2)
+    {
+        result = result + arr[i];
+    }
+    return result;
+}
+
+int[] array = new int[5];
+Random(array);
+Print(array);
+Console.WriteLine(SumNeChet(array));
