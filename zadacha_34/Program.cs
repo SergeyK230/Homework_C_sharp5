e0f8ada9718699c7e84c35e38eb8754d9a292a73
sergey.k <k.sergey2010@yandex.ru>
2022-04-25 20:13:17 +0700

    zadacha_34 done

---
+++ zadacha_34/Program.cs
@@ -0,0 +1,32 @@
+﻿void Random (int[]arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(100, 1000);
+    }
+}
+
+void Print (int[]arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int Chet (int[]arr)
+{
+    int result = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] % 2 == 0)
+            result++;
+    }
+    return result;
+}
+
+int[] array = new int[5];
+Random(array);
+Print(array);
+Console.WriteLine(Chet(array));
