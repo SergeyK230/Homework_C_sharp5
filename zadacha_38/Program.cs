1b0985f31b2954543f2160cc7c0801f122f3dc24
sergey.k <k.sergey2010@yandex.ru>
2022-04-25 20:40:48 +0700

    zadacha_38 done

---
+++ zadacha_38/Program.cs
@@ -0,0 +1,49 @@
+﻿void Random (double[]arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(100, 1000) / 10.0;
+    }
+}
+
+void Print (double[]arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+double Min(double[]arr)
+{
+    double result = arr[0];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] < result)
+            result = arr[i];
+    }
+    return result;
+}
+
+double Max(double[]arr)
+{
+    double result = arr[0];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > result)
+            result = arr[i];
+    }
+    return result;
+}
+
+double Raznica(double[]arr)
+{
+    return Max(arr) - Min(arr);
+}
+
+double[] array = new double[5];
+Random(array);
+Print(array);
+double raz = Raznica(array);
+System.Console.WriteLine("{0:f}", raz);
